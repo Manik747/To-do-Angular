0f0b764e67530e6b647506d63fbe867ca412ddc8
Manik747 <m.martin747@gmail.com>
2019-03-18 00:52:14 +1000

    when you don't make granular commits cause Home PC didn't have git

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyB9Fl3VAq1y9C8Si6asztiJ4Z-7BtBDxYQ",
+    authDomain: "todolist-39f1d.firebaseapp.com",
+    databaseURL: "https://todolist-39f1d.firebaseio.com",
+    projectId: "todolist-39f1d",
+    storageBucket: "todolist-39f1d.appspot.com",
+    messagingSenderId: "86768057093"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
